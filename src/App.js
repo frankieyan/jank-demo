025355f6e6c29e841ca07f5fbdce8ad7425d95d3
Frankie Yan <frankieyan@gmail.com>
2019-09-19 22:09:11 +0800

    Position rendered elements randomly on canvas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, createRef } from 'react';
 import styled from 'styled-components'
 import { Button } from './button/button.component'
 import { Loader } from './loader/loader.component'
@@ -24,17 +24,47 @@ const Canvas = styled.div`
   bottom: 0;
 `
 
+const Element = styled(Loader)`
+  position: absolute;
+  visibility: ${props => props.isProtoType ? 'hidden' : 'visible'};
+  top: ${props => props.top || 'auto'};
+  left: ${props => props.left || 'auto'};
+`
+
 const incrementor = 10;
 const distance = 3;
 const minimum = 10;
+let maxHeight = 0
 
-const createElementIndices = length => new Array(length).fill(null).map((item, index) => index)
+const createElementIndices = length => new Array(length).fill(null).map((_item, index) => ({ id: index }))
 
 function App() {
   const [elements, setElements] = useState(createElementIndices(minimum))
+  const canvasRef = createRef()
+  const protoTypeRef = createRef()
+
   const decrementDisabled = elements.length <= minimum
-  const increment = () => setElements(createElementIndices(elements.length + incrementor))
-  const decrement = () => !decrementDisabled && setElements(createElementIndices(elements.length - incrementor))
+  const increment = () => setInitialElements(elements.length + incrementor)
+  const decrement = () => !decrementDisabled && setInitialElements(elements.length - incrementor)
+
+  const setInitialElements = (length = null) => {
+    const canvasSize = canvasRef.current.getBoundingClientRect()
+    const protoTypeSize = protoTypeRef.current.getBoundingClientRect()
+    const maxWidth = Math.floor(canvasSize.width - protoTypeSize.width) / canvasSize.width * 100
+    maxHeight = Math.floor(canvasSize.height - protoTypeSize.height)
+
+    setElements(createElementIndices(length || elements.length).map(({ id }) => {
+      const top = Math.floor(Math.random() * (maxHeight))
+      const direction = top === maxHeight ? 'up' : 'down'
+      const left = `${(id / (elements.length / maxWidth))}vw`
+
+      return { id, top: `${top}px`, left, direction }
+    }))
+  }
+
+  useEffect(() => {
+    setInitialElements()
+  }, []);
 
   return (
     <React.Fragment>
@@ -45,9 +75,10 @@ function App() {
         <Button>Optimize</Button>
       </SideBar>
 
-      <Canvas>
+      <Canvas ref={canvasRef}>
+        <Element ref={protoTypeRef} isProtoType />
         {
-          elements.map(id => <Loader key={id} />)
+          elements.map(({ id, top, left }) => <Element key={id} top={top} left={left} />)
         }
       </Canvas>
     </React.Fragment>
